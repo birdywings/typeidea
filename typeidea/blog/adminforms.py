0758b58fc785f4b886b739cbac5e68682d26ac57
lvwenqi <769972594@qq@com>
2018-11-10 17:46:53 +0700

    fix

--- typeidea/blog/adminforms.py
+++ typeidea/blog/adminforms.py
@@ -13,7 +13,7 @@ class PostAdminForm(forms.ModelForm):
     desc = forms.CharField(widget=forms.Textarea, label='摘要', required=False)
     # content = forms.CharField(widget=CKEditorWidget(), label="内容")
     # content = forms.CharField(widget=CKEditorUploadingWidget(), label="内容")
-    content = forms.CharField(widget=forms.HiddenInput(), required=False, label="内容")
+    content = forms.CharField(label="内容")
     # category = forms.ModelChoiceField(
     #     queryset=Category.objects.all(),
     #     widget=autocomplete.ModelSelect2(url='category-autocomplete'),
