594edd1f6ca3afb16e15a7e778fe251aeada76a0
lvwenqi <769972594@qq@com>
2019-03-06 15:00:59 +0800

    test post

--- typeidea/blog/app/v1/views.py
+++ typeidea/blog/app/v1/views.py
@@ -79,13 +79,8 @@ class TestPostView(ListCreateAPIView):
     def post(self, request, *args, **kwargs):
         print(request.data['id'])  # POST的数据
 
-        print('test error')
-        a = {'a': 1, 'b': 2}
-        print(a['a'])
-        print(a['c'])
-
         from django.http import JsonResponse
-        return JsonResponse({'code': 0, 'data': '111'})
+        return JsonResponse({'code': 0, 'data': '111', 're_data': request.data})
 
         # return self.response({'code': 0, 'data': '111'})
 
